78e387ab53a495d9d0093fe38c3bf61616ccdec4
kaynam23 <60186448+kaynam23@users.noreply.github.com>
2022-04-21 15:45:51 -0400

    Add files via upload

---
+++ db/db_connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const dbConfig = {
+    host: "soccerdb.calingaiy4id.us-east-2.rds.amazonaws.com",
+    port: 3306,
+    user: kaynam23,
+    password: tk7gb88byzkm,
+    database: "webapp2122t3_kaynam23",
+    connectTimeout: 10000
+}
+
+const connection = mysql.createConnection(dbConfig);
+
+module.exports = connection;
